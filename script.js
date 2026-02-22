7f20ef4f262736aab46dfe7ba4d2df9be9470c44
Jonnas Cipriano <jonnascipriano@gmail.com>
2026-02-22 14:06:09 -0300

    Script finalizado

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// Seleciona os elementos do DOM
 const novaTarefa = document.getElementById("nova-tarefa");
 const adicionarTarefa = document.getElementById("adicionar-tarefa");
 const listaTarefas = document.getElementById("lista-tarefas");
 
+// Variáveis globais
 let id = 0
 let tarefas = [{}];
 
+// Adiciona um evento de clique ao botão "Adicionar Tarefa"
 adicionarTarefa.addEventListener("click", (event) => {
     event.preventDefault();
 
+    // Incrementa o ID da tarefa
     id++;
 
+    // Verifica se a tarefa não está vazia
     if (novaTarefa.value.trim() !== "") {
         tarefas.push({
             id: id,
@@ -17,18 +22,32 @@ adicionarTarefa.addEventListener("click", (event) => {
             concluida: false
         })
 
+        // Cria um novo item de tarefa
         const tarefaItem = document.createElement("li");
         tarefaItem.classList.add("tarefa");
-        tarefaItem.setAttribute("id", id);
+        tarefaItem.setAttribute("id", tarefas.length - 1);
         tarefaItem.innerHTML = `<p>
                                     <input type="checkbox"">${novaTarefa.value.trim()}
                                 </p>
                                 <button><img src="./img/excluir.png" alt="lixeira"></button>`;
         listaTarefas.appendChild(tarefaItem);
+
+        // Adiciona o evento de clique para marcar a tarefa como concluída
+        const checkbox = tarefaItem.querySelector("input[type='checkbox']");
+        checkbox.addEventListener('change', () => {
+        tarefaItem.classList.toggle('feito')
+        })
+
+        // Adiciona o evento de clique para excluir a tarefa
+        const botaoExcluir = tarefaItem.querySelector('button')
+        botaoExcluir.addEventListener('click', () => {
+            tarefaItem.remove();
+        })
+
         novaTarefa.value = "";
     } else {
+        // Exibe um alerta se a tarefa estiver vazia
         alert("Por favor, insira uma tarefa válida.");
     }
-
-    console.log(tarefas);
 });
+
